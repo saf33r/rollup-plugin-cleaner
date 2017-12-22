af993dc911969f90b1e56946de1700f52a490273
Karolis Grinkevičius <k.grinkevicius@gmail.com>
2017-12-22 11:59:11 +0200

    Silent option added to not log into console

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var rimraf = require('rimraf');
 module.exports = function(options) {
   options = options || {};
   var targets = options.targets ? options.targets : [];
+  var silent = options.silent === true;
   return {
     name: 'cleaner',
     ongenerate: function(details) {
@@ -15,7 +16,7 @@ module.exports = function(options) {
         for (let i = 0; i < targets.length; i++) {
           normalisedPath = path.normalize(targets[i]);
           if (fs.existsSync(normalisedPath)) {
-            console.log('cleaning path: ' + normalisedPath);
+            !silent && console.log('cleaning path: ' + normalisedPath);
             rimraf.sync(normalisedPath);
           }
         }
