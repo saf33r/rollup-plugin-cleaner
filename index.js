f145050f4094472f6ed4809776124e99b6cfb30e
Paul Melnikow <email@paulmelnikow.com>
2019-07-01 13:53:26 -0400

    Set up prettier

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-"use strict";
+'use strict'
 
-var fs = require('fs');
-var path = require('path');
-var rimraf = require('rimraf');
+var fs = require('fs')
+var path = require('path')
+var rimraf = require('rimraf')
 
 module.exports = function(options) {
-  options = options || {};
-  var targets = options.targets ? options.targets : [];
-  var silent = options.silent === true;
+  options = options || {}
+  var targets = options.targets ? options.targets : []
+  var silent = options.silent === true
   return {
     name: 'cleaner',
     ongenerate: function(details) {
-      var normalisedPath;
+      var normalisedPath
       if (targets && targets.length) {
         for (let i = 0; i < targets.length; i++) {
-          normalisedPath = path.normalize(targets[i]);
+          normalisedPath = path.normalize(targets[i])
           if (fs.existsSync(normalisedPath)) {
-            !silent && console.log('cleaning path: ' + normalisedPath);
-            rimraf.sync(normalisedPath);
+            !silent && console.log('cleaning path: ' + normalisedPath)
+            rimraf.sync(normalisedPath)
           }
         }
       }
-    }
+    },
   }
 }
